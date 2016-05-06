9b82fe46e4f3b43110a5a4b15966e0be5a30c99c
linconcp <linconcp@gmail.com>
2016-05-05 23:47:30 -0300

    teste

---
+++ app/src/main/java/br/ufg/prograd/centroaula/entidade/EHorario.java
@@ -0,0 +1,59 @@
+package br.ufg.prograd.centroaula.entidade;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity(name = "HORARIO")
+@Table(name = "horario")
+public class EHorario implements Serializable {
+
+  private static final long serialVersionUID = -2591154974918066513L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  private Date dataInicio;
+
+  private Date dataFim;
+
+  private String[] disciplinaSala;
+
+  public Integer getId() {
+    return this.id;
+  }
+
+  public void setId(final Integer id) {
+    this.id = id;
+  }
+
+  public Date getDataInicio() {
+    return this.dataInicio;
+  }
+
+  public void setDataInicio(final Date dataInicio) {
+    this.dataInicio = dataInicio;
+  }
+
+  public Date getDataFim() {
+    return this.dataFim;
+  }
+
+  public void setDataFim(final Date dataFim) {
+    this.dataFim = dataFim;
+  }
+
+  public String[] getDisciplinaSala() {
+    return this.disciplinaSala;
+  }
+
+  public void setDisciplinaSala(final String[] sala) {
+    this.disciplinaSala = sala;
+  }
+}
